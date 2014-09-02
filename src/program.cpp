fc159cec416c6ff4bece9a4a3b0edf9c9f490932
eugene <eugene@Arkady>
2014-09-02 15:17:47 +0600

    TestChange

--- src/program.cpp
+++ src/program.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-	DEV_Generator ss("/dev/random");
+	DEV_Generator ss("/dev/urandom");
 	IRandomGenerator *dd = &ss;
 	cout << dd->getRandom_01() << endl;
 	cout << (int) dd->getMinMaxRandom(1, 65534) << endl;
